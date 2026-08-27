739d4eb5a53704f000d3a66049fb45cd10077937
Lodewyk Roux <lloderoux@protonmail.ch>
2026-08-27 14:59:16 +0200

    Fixed company name.

--- src/components/PortfolioPage.tsx
+++ src/components/PortfolioPage.tsx
@@ -60,7 +60,7 @@ export function PortfolioPage() {
                                 <h3 className="text-base font-bold text-[#0e3d39]">
                                     Senior Software Developer
                                 </h3>
-                                <p className="text-xs font-semibold text-[#d99b26]">UPS</p>
+                                <p className="text-xs font-semibold text-[#d99b26]">UFS</p>
                                 <p className="text-xs text-slate-400">May 2025 - Current</p>
                                 <p className="text-xs text-slate-400">Hybrid, Bloemfontein</p>
                             </div>
